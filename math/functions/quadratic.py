6c032ec06e3c6ba389382bc9c7854a6c449a6182
John Doe <46622921+TheRandomOwl@users.noreply.github.com>
2024-04-05 16:48:20 -0700

    add quadratic solver

---
+++ math/functions/quadratic.py
@@ -0,0 +1,12 @@
+# Calculates the solution to the quadratic formula
+import math
+try:
+    A = float(input("Enter a number for A: "))
+    B = float(input("Enter a number for B: "))
+    C = float(input("Enter a number for C: "))
+except:
+    print("Invalid input")
+else:
+    ans1 = (-B + math.sqrt(B*B - 4*A*C)) / 2*A
+    ans2 = (-B - math.sqrt(B*B - 4*A*C)) / 2*A
+    print(f"{ans1}, {ans2}")
