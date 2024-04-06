0427fbf92ab08902f3acbeff30e3433b1d17c615
John Doe <46622921+TheRandomOwl@users.noreply.github.com>
2024-04-06 11:53:24 -0700

    Add Egyptian Multiplication

---
+++ math/Egyptian-multiplication/Egyptian.py
@@ -0,0 +1,19 @@
+# Egyptian Multiplication
+
+ans = []
+while True:
+    try:   
+        a = int(input("Enter a number: "))
+        b = int(input("Enter a number: "))
+        break
+    except ValueError:
+        print("Invalid input")
+while (a >= 1 and a > 0) or (a <= -1 and a < 0):
+    if (a % 2 != 0 and a > 0):
+        ans.append(b)
+    elif (a % 2 != 0 and a < 0):
+        ans.append(-b)
+    a = int(a/2)
+    b *= 2
+print(ans)
+print(f"Answer: {sum(ans)}")
