6fe50174444459af38286c3cd2b64e707c6b11fd
John Doe <46622921+TheRandomOwl@users.noreply.github.com>
2024-04-06 18:02:41 -0700

    finish game

--- games/tic-tac-toe.py
+++ games/tic-tac-toe.py
@@ -1,31 +1,55 @@
 # tic-tac-toe game
-player1 = {
-    "name": 'Player 1',
-    "row": 0,
-    "column": 0,
-    "symbol": 'X'
-}
-player2 = {
-    "name": 'Player 2',
-    "row": 0,
-    "column": 0,
-    "symbol": 'O'
-}
 
 grid = [[' ',' ',' '],
         [' ',' ',' '],
         [' ',' ',' ']
         ]
-def enter_move(player):
-    player["row"] = int(input(f"{player['name']} enter a row: "))
-    player["column"] = int(input(f"{player['name']} enter a column: "))
-    grid[player["row"]][player["column"]] = player["symbol"]
+def enter_move(player, symbol):
+    show_grid()
+    while True:
+        try:
+            row = int(input(f"{player} enter a row: "))
+            column = int(input(f"{player} enter a column: "))
+            if grid[row][column] == ' ':
+                grid[row][column] = symbol
+                break
+            else:
+                print("This location is already taken.")
+        except IndexError:
+            print("Invalid range")
+        except ValueError:
+            print("Please enter a number")
 
 def show_grid(): # 3 x 3 grid
     print(f"╔═══╦═══╦═══╗\n║ {grid[0][0]} ║ {grid[0][1]} ║ {grid[0][2]} ║\n╠═══╬═══╬═══╣\n║ {grid[1][0]} ║ {grid[1][1]} ║ {grid[1][2]} ║\n╠═══╬═══╬═══╣\n║ {grid[2][0]} ║ {grid[2][1]} ║ {grid[2][2]} ║\n╚═══╩═══╩═══╝")
 
-show_grid()
-for i in range(9):
-    enter_move(player1)
-    enter_move(player2)
+def check_win():
+    game_over = False
+    # Check rows
+    for row in grid:
+        if (row[0] == row[1] == row[2] and row[0] != ' '):
+            game_over = True
+
+    # Check columns
+    for col in range(3):
+        if (grid[0][col] == grid[1][col] == grid[2][col] and grid[0][col] != ' '):
+            game_over = True
+
+    # Check diagonals
+    if (grid[0][0] == grid[1][1] == grid[2][2] or grid[0][2] == grid[1][1] == grid[2][0]) and (grid[1][1] != ' '):
+        game_over = True
+        
     show_grid()
+    return game_over
+
+for i in range(5):
+    enter_move('Player 1','X')
+    if check_win():
+        print("You win!")
+        break
+    if (i < 4): enter_move('Player 2','O') # prevent player 2 from getting stuck in loop
+    if check_win():
+        print("You lose!")
+        break
+else:
+    print("Tie")
