836025e17f93e57cf91b1645d251f909295c038a
John Doe <46622921+TheRandomOwl@users.noreply.github.com>
2024-04-18 09:02:06 -0700

    add function to print board

--- games/connect-4.py
+++ games/connect-4.py
@@ -1,8 +1,20 @@
 # Connect Four
 
-# 1. Create a 6x7 grid
-# 2. Create a function to print the grid using Box-drawing character
+# create an array to store the board   
+board = [[0] for x in range(7)]
 
-grid = [[' ' for _ in range(7)] for _ in range(6)]
-
-print(grid)
+def print_board():
+    for y in range(6):
+        print("|", end="")
+        for x in range(7):
+            if len(board[x]) > y:
+                print(board[x][y], end="")
+            else:
+                print(" ", end="")
+            print("|", end="")
+        print()
+    print("---------------")
+    print(" 1 2 3 4 5 6 7")
+print_board()
+print(board)
+print(len(board[0]))
